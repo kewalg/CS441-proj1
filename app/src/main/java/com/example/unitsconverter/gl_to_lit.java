1bbc6d28250ee9d7ebf178ad20cf337a635e3530
kewalg <kewalgulve007@gmail.com>
2019-09-03 12:20:44 -0400

    Basic UI

---
+++ app/src/main/java/com/example/unitsconverter/gl_to_lit.java
@@ -0,0 +1,14 @@
+package com.example.unitsconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gl_to_lit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gl_to_lit);
+    }
+}
---
+++ app/src/main/java/com/example/unitsconverter/lbs_to_kgs.java
@@ -0,0 +1,14 @@
+package com.example.unitsconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lbs_to_kgs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lbs_to_kgs);
+    }
+}
